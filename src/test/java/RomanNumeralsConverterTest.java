e5ff91398e91335390bccd85a7dcb5745a8f9863
abcpaem <abcpaem@gmail.com>
2022-03-22 11:54:13 +0000

    Refactors to use ParameterizedTest and therefore apply the DRY principle

--- src/test/java/RomanNumeralsConverterTest.java
+++ src/test/java/RomanNumeralsConverterTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,53 +12,9 @@ class RomanNumeralsConverterTest {
         assertEquals(0, RomanNumeralsConverter.getNumber(""));
     }
 
-    @Test
-    void checkGetNumberWhenRomanNumeralIsOne() {
-        assertEquals(1, RomanNumeralsConverter.getNumber("I"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsTwo() {
-        assertEquals(2, RomanNumeralsConverter.getNumber("II"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsThree() {
-        assertEquals(3, RomanNumeralsConverter.getNumber("III"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsFour() {
-        assertEquals(4, RomanNumeralsConverter.getNumber("IV"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsFive() {
-        assertEquals(5, RomanNumeralsConverter.getNumber("V"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsSix() {
-        assertEquals(6, RomanNumeralsConverter.getNumber("VI"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsSeven() {
-        assertEquals(7, RomanNumeralsConverter.getNumber("VII"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsEight() {
-        assertEquals(8, RomanNumeralsConverter.getNumber("VIII"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsNine() {
-        assertEquals(9, RomanNumeralsConverter.getNumber("IX"));
-    }
-
-    @Test
-    void checkGetNumberWhenRomanNumeralIsTen() {
+    @ParameterizedTest
+    @ValueSource(strings = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"})
+    void checkGetNumberWhenRomanNumeralIsUpToTen() {
         assertEquals(10, RomanNumeralsConverter.getNumber("X"));
     }
 
